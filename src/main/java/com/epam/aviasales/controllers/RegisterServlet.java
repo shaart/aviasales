432b459fdf99162ce758bda5bea3a597e58500ca
Artur Shalaev <imshaart@gmail.com>
2018-04-12 14:41:39 +0300

    JSP fix: web-app version, MainServlet default now, annotations removed
    Web-app version now 3.0.
    From servlets removed annotation and added mapping to web.xml.
    index.jsp get from servlet and show "pageName".

--- src/main/java/com/epam/aviasales/controllers/RegisterServlet.java
+++ src/main/java/com/epam/aviasales/controllers/RegisterServlet.java
@@ -1,8 +1,17 @@
 package com.epam.aviasales.controllers;
 
-import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+
+    super.doGet(req, resp);
+  }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,6 +1,6 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <body>
-<h2>Hello World!</h2>
+<h1>Welcome to ${pageName}!</h1>
 </body>
 </html>
