568baafbd6cd0ad36bb165ca0290bb81d1243f3b
Pavel Fomkin <pavel_fomkin@epam.com>
2018-04-12 18:03:56 +0300

    add airport and flight classes and xml entities

--- src/main/java/com/epam/aviasales/controllers/LoginServlet.java
+++ src/main/java/com/epam/aviasales/controllers/LoginServlet.java
@@ -1,11 +1,13 @@
 package com.epam.aviasales.controllers;
 
-import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
   @Override
