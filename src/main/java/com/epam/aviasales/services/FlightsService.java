dadd5aa54b84f1298fca378803bc052013da0343
Artur Shalaev <imshaart@gmail.com>
2018-04-13 21:27:36 +0300

    FlightService with mocked getFlights()

---
+++ src/main/java/com/epam/aviasales/services/FlightsService.java
@@ -0,0 +1,9 @@
+package com.epam.aviasales.services;
+
+import com.epam.aviasales.domain.Flight;
+import java.util.List;
+
+public interface FlightsService {
+
+  public List<Flight> getFlights();
+}
