a5f2baf0a8b54f1863a7ff2a57152b222918f25d
Artur Shalaev <imshaart@gmail.com>
2018-04-15 21:00:12 +0300

    FlightsService & FlightsServiceImpl removed (old files)

--- src/main/java/com/epam/aviasales/services/FlightsService.java
+++
@@ -1,9 +0,0 @@
-package com.epam.aviasales.services;
-
-import com.epam.aviasales.domain.Flight;
-import java.util.List;
-
-public interface FlightsService {
-
-  public List<Flight> getFlights();
-}
