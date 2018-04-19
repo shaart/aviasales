3d85756fdd15035bf7363ada5eaf5cadfe69c4da
Artur Shalaev <imshaart@gmail.com>
2018-04-19 18:12:29 +0300

    ManageFlightsServlet - updated for ParseRequestHelper

--- src/main/java/com/epam/aviasales/controllers/ManageFlightsServlet.java
+++ src/main/java/com/epam/aviasales/controllers/ManageFlightsServlet.java
@@ -12,6 +12,7 @@ import com.epam.aviasales.services.impl.AirportServiceImpl;
 import com.epam.aviasales.services.impl.FlightServiceImpl;
 import com.epam.aviasales.services.impl.ParserServiceImpl;
 import com.epam.aviasales.util.Action;
+import com.epam.aviasales.util.ParseRequestHelper;
 import java.io.IOException;
 import java.util.List;
 import javax.servlet.ServletException;
@@ -47,16 +48,16 @@ public class ManageFlightsServlet extends HttpServlet {
       final int DEFAULT_PAGE_SIZE = 15;
       final int DEFAULT_PAGE = 1;
 
-      int page = getParameterOrDefaultValue(req, "page", DEFAULT_PAGE);
-      int size = getParameterOrDefaultValue(req, "size", DEFAULT_PAGE_SIZE);
+      int page = ParseRequestHelper.getParameterOrDefaultValue(req, "page", DEFAULT_PAGE);
+      int size = ParseRequestHelper.getParameterOrDefaultValue(req, "size", DEFAULT_PAGE_SIZE);
       req.setAttribute("page", page);
       req.setAttribute("size", size);
 
       String[] parameters = {"id", "fromAirport", "toAirport", "airplane", "departureTime",
           "arrivalTime", "baseTicketPrice", "extraBaggagePrice", "freeSeatEconomy",
           "freeSeatBusiness"};
-      setExistingParametersAsAttributes(req, parameters);
-      setPagingURLAttributes(req, page);
+      ParseRequestHelper.setExistingParametersAsAttributes(req, parameters);
+      ParseRequestHelper.setPagingURLAttributes(req, page);
 
       Flight seekingFlight = parserService.parseFlight(req);
       List<Flight> flights = flightService.getFlightsLike(seekingFlight, page, size);
@@ -80,7 +81,7 @@ public class ManageFlightsServlet extends HttpServlet {
   protected void doPost(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
 
-    Action action = getAction(req);
+    Action action = ParseRequestHelper.getRequestAction(req);
     try {
       Flight receivedFlight = parserService.parseFlight(req);
 
@@ -111,64 +112,5 @@ public class ManageFlightsServlet extends HttpServlet {
     } catch (IOException e) {
       log.error(e);
     }
-  }
-
-  private Action getAction(HttpServletRequest req) {
-    Action action = Action.NONE;
-    String actionAdd = req.getParameter("actionAdd");
-    String actionSave = req.getParameter("actionSave");
-    String actionDelete = req.getParameter("actionDelete");
-    if (actionAdd != null && !actionAdd.isEmpty()) {
-      action = Action.ADD;
-    } else if (actionSave != null && !actionSave.isEmpty()) {
-      action = Action.SAVE;
-    } else if (actionDelete != null && !actionDelete.isEmpty()) {
-      action = Action.DELETE;
-    }
-    return action;
-  }
-
-  private void setExistingParametersAsAttributes(HttpServletRequest request, String[] parameters) {
-    for (String parameter : parameters) {
-      String parameterValue = request.getParameter(parameter);
-      if (parameterValue != null && !parameterValue.trim().isEmpty()) {
-        request.setAttribute(parameter, parameterValue);
-      }
-    }
-  }
-
-  private int getParameterOrDefaultValue(HttpServletRequest req, String parameter,
-      final int DEFAULT) {
-    String paramStr = req.getParameter(parameter);
-    return paramStr == null ? DEFAULT : Integer.parseInt(paramStr);
-  }
-
-  private void setPagingURLAttributes(HttpServletRequest req, int page) {
-    String reqParameters = req.getQueryString();
-    if (reqParameters == null) {
-      reqParameters = "?";
-    } else {
-      reqParameters = "?" + reqParameters;
-    }
-    String prevPage;
-    String currPage;
-    String nextPage;
-    if (reqParameters.contains("page")) {
-      prevPage = reqParameters.replaceFirst("page=[^&]*", "page=" + (page - 1));
-      currPage = reqParameters;
-      nextPage = reqParameters.replaceFirst("page=[^&]*", "page=" + (page + 1));
-    } else {
-      prevPage = reqParameters + "&page=" + (page - 1);
-      currPage = reqParameters + "&page=" + (page);
-      nextPage = reqParameters + "&page=" + (page + 1);
-    }
-
-    req.setAttribute("prevPageURL", prevPage);
-    req.setAttribute("currPageURL", currPage);
-    req.setAttribute("nextPageURL", nextPage);
-  }
-
-  private boolean isNullOrEmpty(String parameter) {
-    return parameter == null || parameter.isEmpty();
   }
 }
